7185937dab7e74aafebe7c5a9f0a87e1e66bb85e
pywd <yuki.wd7@gmail.com>
2021-05-01 18:40:13 +0900

    add disk manager

---
+++ src/disk.rs
@@ -0,0 +1,62 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, Read, Seek, SeekFrom, Write},
+    path::Path,
+    usize,
+};
+
+pub const PAGE_SIZE: usize = 4096;
+pub struct DiskManager {
+    heap_file: File,
+    next_page_id: u64,
+}
+impl DiskManager {
+    pub fn new(heap_file: File) -> io::Result<Self> {
+        let heap_file_size = heap_file.metadata()?.len();
+        let next_page_id = heap_file_size / PAGE_SIZE as u64;
+        Ok(Self {
+            heap_file,
+            next_page_id,
+        })
+    }
+
+    // ヒープファイルを開く
+    pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
+        let heap_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(heap_file_path)?;
+        Self::new(heap_file)
+    }
+
+    // 新しいページを作る
+    pub fn allocate_page(&mut self) -> PageId {
+        let page_id = self.next_page_id;
+        // 次のページIDを採番
+        self.next_page_id += 1;
+        PageId(page_id)
+    }
+
+    // ページに書き込む
+    pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
+        // ページ先頭へシーク
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        self.heap_file.write_all(data)
+    }
+
+    // ページから読み込む
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        self.heap_file.read_exact(data)
+    }
+}
+
+pub struct PageId(pub u64);
+impl PageId {
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}

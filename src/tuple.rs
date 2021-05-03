0d9c7306e67d9044d569aaba50062b376182b9d9
pywd <yuki.wd7@gmail.com>
2021-05-03 18:57:30 +0900

    add query to show all records in simple table

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Debug};
+
 use crate::memcomparable;
 
 // memcomparable formatでエンコード
@@ -18,4 +20,24 @@ pub fn decode(bytes: &[u8], elems: &mut Vec<Vec<u8>>) {
     memcomparable::decode(&mut rest, &mut elem);
     elems.push(elem);
   }
+}
+
+pub struct Pretty<'a, T>(pub &'a [T]);
+
+impl<'a, T: AsRef<[u8]>> Debug for Pretty<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut d = f.debug_tuple("Tuple");
+        for elem in self.0 {
+            let bytes = elem.as_ref();
+            match std::str::from_utf8(&bytes) {
+                Ok(s) => {
+                    d.field(&format_args!("{:?} {:02x?}", s, bytes));
+                }
+                Err(_) => {
+                    d.field(&format_args!("{:02x?}", bytes));
+                }
+            }
+        }
+        d.finish()
+    }
 }
